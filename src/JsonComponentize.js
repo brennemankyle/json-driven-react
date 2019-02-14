2e324c9a6040e8ab2d459966a9a85db892d49eac
Kyle Brenneman <kbrenneman@athn.org>
2019-02-14 14:58:49 -0700

    Add error for not being able to compontetize

--- src/JsonComponentize.js
+++ src/JsonComponentize.js
@@ -7,9 +7,15 @@ class JsonComponentize {
   translate(json) {
     if (Array.isArray(json) || (json && typeof json === 'object')) {
       ([].concat(json).forEach((item, index) => {
-        item.element = typeof item.element === 'string' && !!item.element.match(/^[A-Z].*/)
-          ? this.translator[item.element]
-          : item.element
+        if (typeof item.element === 'string' && !!item.element.match(/^[A-Z].*/)) {
+          let component = this.translator[item.element]
+
+          if (!component) throw new Error('Cannot find component named "' + item.element + '" in componentized translator'')
+
+          item.element = component
+        } else {
+          item.element = item.element
+        }
 
         this.translate(item.children)
       }))
